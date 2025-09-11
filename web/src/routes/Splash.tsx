b8a9c44c5d7928389ccc370f82961fc5860add13
JosephSCH0157 <Jschramer@gmail.com>
2025-09-10 22:57:50 -0400

    Implement code changes to enhance functionality and improve performance

--- web/src/routes/Splash.tsx
+++ web/src/routes/Splash.tsx
@@ -1,35 +1,69 @@
 import { Link } from "react-router-dom";
 import "./Splash.css"; // optional external CSS
 
+
 export default function Splash() {
   return (
-    <div className="splash-container">
-      <img src="/src/assets/forge.png" alt="Forge" className="splash-bg" />
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100vh", // take full viewport height
+        overflow: "hidden",
+      }}
+    >
+      <img src="/forge.png" alt="Forge" />
 
       {/* Hotspots */}
-      <Link to="/tools/forge" className="hotspot" style={{ top: "55%", left: "70%" }}>
+      <Link
+        to="/tools/forge"
+        className="hotspot"
+        style={{ top: "55%", left: "70%" }}
+      >
         Forge
       </Link>
-      <Link to="/tools/anvil" className="hotspot" style={{ top: "70%", left: "40%" }}>
+      <Link
+        to="/tools/anvil"
+        className="hotspot"
+        style={{ top: "70%", left: "40%" }}
+      >
         Anvil
       </Link>
-      <Link to="/tools/hammer" className="hotspot" style={{ top: "15%", left: "35%" }}>
+      <Link
+        to="/tools/hammer"
+        className="hotspot"
+        style={{ top: "15%", left: "35%" }}
+      >
         Hammer
       </Link>
-      <Link to="/tools/tongs" className="hotspot" style={{ top: "50%", left: "10%" }}>
+      <Link
+        to="/tools/tongs"
+        className="hotspot"
+        style={{ top: "50%", left: "10%" }}
+      >
         Tongs
       </Link>
-      <Link to="/tools/quench" className="hotspot" style={{ top: "70%", left: "80%" }}>
+      <Link
+        to="/tools/quench"
+        className="hotspot"
+        style={{ top: "70%", left: "80%" }}
+      >
         Quench
       </Link>
-      <Link to="/tools/ledger" className="hotspot" style={{ top: "80%", left: "65%" }}>
+      <Link
+        to="/tools/ledger"
+        className="hotspot"
+        style={{ top: "80%", left: "65%" }}
+      >
         Ledger
       </Link>
-      <Link to="/tools/stock" className="hotspot" style={{ top: "85%", left: "20%" }}>
+      <Link
+        to="/tools/stock"
+        className="hotspot"
+        style={{ top: "85%", left: "20%" }}
+      >
         Bar Stock
       </Link>
-
-      <div className="brand">Creator’s Forge</div>
     </div>
   );
 }
