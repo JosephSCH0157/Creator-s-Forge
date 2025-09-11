2d5074129c875f672620fc4746af0297f49f93e3
JosephSCH0157 <Jschramer@gmail.com>
2025-09-11 08:19:55 -0400

    Implement code changes to enhance functionality and improve performance

--- web/src/routes/Anvil.jsx
+++ web/src/routes/Anvil.jsx
@@ -1,18 +1,42 @@
+import { Link } from "react-router-dom";
+
 export default function Anvil() {
   return (
-    <iframe
-      src="/teleprompter_pro.html"
-      title="Teleprompter Pro"
-      style={{
-        position: "fixed",
-        inset: 0,           // top:0 right:0 bottom:0 left:0
-        width: "100vw",
-        height: "100vh",
-        border: "none",
-        display: "block",
-      }}
-      allow="clipboard-read; clipboard-write; fullscreen"
-      allowFullScreen
-    />
+    <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
+      {/* Top bar */}
+      <div
+        style={{
+          height: 48,
+          display: "flex",
+          alignItems: "center",
+          gap: 8,
+          padding: "0 16px",
+          background: "#111827",
+          color: "white",
+          borderBottom: "1px solid #333",
+        }}
+      >
+        <img
+          src="/anvil.png"
+          alt="Anvil"
+          style={{ height: 24, width: 24, objectFit: "contain" }}
+        />
+        <Link
+          to="/"
+          style={{ color: "white", textDecoration: "none", fontWeight: 600 }}
+        >
+          Back to Creator’s Forge
+        </Link>
+      </div>
+
+      {/* Iframe */}
+      <iframe
+        src="/teleprompter_pro.html"
+        title="Teleprompter Pro"
+        style={{ flex: 1, border: "none", width: "100%" }}
+        allow="clipboard-read; clipboard-write; fullscreen"
+        allowFullScreen
+      />
+    </div>
   );
 }
