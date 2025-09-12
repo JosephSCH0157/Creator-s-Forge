c13818e1d974fc2ae5b43cff283a0711fa048138
JosephSCH0157 <Jschramer@gmail.com>
2025-09-12 09:22:18 -0400

    Update hearth position in SplashRoute component

--- web/src/routes/Splash.tsx
+++ web/src/routes/Splash.tsx
@@ -31,7 +31,7 @@ export default function SplashRoute() {
          - Uses /public/forge.png. Change if your file lives elsewhere. */}
    <SplashWithEmbers
   src={`${import.meta.env.BASE_URL}forge.png`}
-  hearth={{ x: 1010, y: 525, w: 220, h: 120 }}
+  hearth={{ x: 1010, y: 515, w: 220, h: 120 }}
   spawnRate={140}
   maxEmbers={400}
   className="absolute inset-0 z-0 pointer-events-none"
