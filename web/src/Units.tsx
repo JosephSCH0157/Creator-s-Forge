2eb1e0cad0324bde160f6a7ce5bd1718c1e83a6d
JosephSCH0157 <Jschramer@gmail.com>
2025-09-15 09:35:06 -0400

    refactor: enhance import statement for improved type clarity in Units component

--- web/src/Units.tsx
+++ web/src/Units.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+import React, { JSX, useEffect, useState } from "react";
 
 const API = "http://127.0.0.1:5177";
 
